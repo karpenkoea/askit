1b3f2a50d28e7ea92314f4c2e0e23b1ae0e629b7
ps4karpenko <ps4karpenko@gmail.com>
2022-03-03 09:29:46 +0300

    basic

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  def nav_link(title, url, options = {})
+    current_page = options.delete :current_page
+    css_class = title == current_page ? 'active' : ''
+    options[:class] = options[:class].present? ? "#{options[:class]} #{css_class}" : css_class
+
+    link_to title, url, options
+  end
+
+  def currently_at(current_page = '')
+    render partial: 'shared/header', locals: {current_page: current_page}
+  end
+
+  def full_title(page_title = '')
+    base_title = 'AskIt'
+    page_title.present? ? "#{page_title} | #{base_title}" : base_title
+  end
 end
