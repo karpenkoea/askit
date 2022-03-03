7b6c3d700d22320ebee8acef3a5c309bd7933f60
ps4karpenko <ps4karpenko@gmail.com>
2022-03-03 14:29:51 +0300

    fix

--- app/controllers/concerns/authentication.rb
+++ app/controllers/concerns/authentication.rb
@@ -31,15 +31,22 @@ module Authentication
     end
 
     def current_user
-      if session[:user_id].present?
-        @current_user ||= User.find_by(id: session[:user_id])
-      elsif cookies.encrypted[:user_id].present?
-        user = User.find_by(id: cookies.encrypted[:user_id])
-        if user&.remember_token_authenticated?(cookies.encrypted[:remember_token])
-          sing_in user
-          @current_user ||= user
-        end
-      end
+      user = session[:user_id].present? ? user_from_session : user_from_remember_token
+      @current_user ||= user unless user.nil?
+    end
+
+    def user_from_session
+      User.find_by(id: session[:user_id])
+    end
+
+    def user_from_remember_token
+      user = User.find_by(id: cookies.encrypted[:user_id])
+      token = cookies.encrypted[:remember_token]
+
+      return unless user&.remember_token_authenticated?(token)
+
+      sing_in user
+      user
     end
 
     def user_signed_in?
