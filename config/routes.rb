2523d679e9cdc96447ea7cc9fab072090145beb0
karpenkoea <ps4karpenko@gmail.com>
2022-03-07 15:59:37 +0300

    change locales

--- config/routes.rb
+++ config/routes.rb
@@ -1,15 +1,16 @@
 Rails.application.routes.draw do
+  scope '(:locale)', locale: /#{I18n.available_locales.join('|')}/ do
+    resources :questions do
+      resources :answers, except: :new
+    end
 
-  resources :questions do
-    resources :answers, except: :new
+    resource :session, only: %i[new create destroy]
+    resources :users, only: %i[new create edit update show]
+
+    namespace :admin do
+      resources :users, only: %i[index create]
+    end
+
+    root 'pages#index'
   end
-
-  resource :session, only: %i[new create destroy]
-  resources :users, only: %i[new create edit update show]
-
-  namespace :admin do
-    resources :users, only: %i[index create]
-  end
-
-  root 'pages#index'
 end
