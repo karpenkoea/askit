d3c465c22a663c59ee5540ca5bd618ce76ad240e
karpenkoea <ps4karpenko@gmail.com>
2022-03-10 15:37:01 +0300

    fixed

--- db/migrate/20220310080622_add_user_id_to_answers.rb
+++ db/migrate/20220310080622_add_user_id_to_answers.rb
@@ -1,5 +1,5 @@
 class AddUserIdToAnswers < ActiveRecord::Migration[6.0]
   def change
-    add_reference :answers, :user, null: false, foreign_key: true, default: User.first.id
+    add_reference :answers, :user, null: false, foreign_key: true
   end
 end
--- db/migrate/20220310080630_add_user_id_to_questions.rb
+++ db/migrate/20220310080630_add_user_id_to_questions.rb
@@ -1,5 +1,5 @@
 class AddUserIdToQuestions < ActiveRecord::Migration[6.0]
   def change
-    add_reference :questions, :user, null: false, foreign_key: true, default: User.first.id
+    add_reference :questions, :user, null: false, foreign_key: true
   end
 end
